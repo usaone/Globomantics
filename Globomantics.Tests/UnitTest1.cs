c30233fe4144df387183045e2f9c416ca5eb06e8
Uddip Mitra <uddipmitra@gmail.com>
2021-06-03 18:25:58 -0400

    Add project files

---
+++ Globomantics.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using Globomantics;
+
+namespace Globomantics.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
